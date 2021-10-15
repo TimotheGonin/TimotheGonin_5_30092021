9c5f545891f9d1e45236b249f3084429f899df39
TimotheGonin <gonin.timothe@gmail.com>
2021-10-15 11:48:51 +0200

    test : create elements, give child

--- js/app.js
+++ js/app.js
@@ -4,3 +4,20 @@
 
 //scope main content box
 const mainContent = document.getElementById('main-content');
+
+//create elements
+let card = document.createElement('article');
+let cardLink = document.createElement('a');
+let cardHeader = document.createElement('div');
+let cardFigure = document.createElement('figure');
+let cardPicture = document.createElement('img');
+
+//give class name 
+card.classList.add('card');
+
+//give child to elements
+mainContent.appendChild(card);
+card.appendChild(cardLink);
+cardLink.appendChild(cardHeader);
+cardHeader.appendChild(cardFigure);
+cardFigure.appendChild(cardPicture);
