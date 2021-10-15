e11236af31a30cf172f9bbdfec1ee4de0fba5f63
TimotheGonin <gonin.timothe@gmail.com>
2021-10-15 14:46:49 +0200

    test : add class name and  set attribute

--- js/app.js
+++ js/app.js
@@ -12,8 +12,12 @@ let cardHeader = document.createElement('div');
 let cardFigure = document.createElement('figure');
 let cardPicture = document.createElement('img');
 
-//give class name 
+//give class name and attribute
 card.classList.add('card');
+cardLink.setAttribute('href','photographer.html');
+cardHeader.classList.add('card__header');
+cardFigure.classList.add('card__figure');
+cardPicture.setAttribute('src', '../public/img/Photographers_ID_Photos/EllieRoseWilkens.jpg');
 
 //give child to elements
 mainContent.appendChild(card);
