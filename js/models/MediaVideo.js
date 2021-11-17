10a72d7951299bc6d0fa5c9aa7523c856c2de18c
TimotheGonin <gonin.timothe@gmail.com>
2021-11-17 08:54:26 +0100

    feat : addiing picture/video template

---
+++ js/models/MediaVideo.js
@@ -0,0 +1,37 @@
+class MediaVideo{
+    constructor(data){
+        this._id = data.id
+        this._photographerId = data.photographerId
+        this._title = data.title
+        this._video = data.video
+        this._tags = data.tags
+        this._likes = data.likes
+        this._date = data.date
+        this._price = data.price
+    }
+
+    get id(){
+        return this._id
+    }
+    get photographerId(){
+        return this._photographerId
+    }
+    get title(){
+        return  this._title
+    }
+    get video(){
+        return this._video
+    }
+    get tags(){
+        return this._tags
+    }
+    get likes(){
+        return this._likes
+    }
+    get date(){
+        return this._date
+    }
+    get price(){
+        return this._price
+    }
+}
