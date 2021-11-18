ad81c1d78a33db4d6ca59c5aa2b20fc142c0938e
TimotheGonin <gonin.timothe@gmail.com>
2021-11-18 11:29:11 +0100

    test : api file module

--- js/Photographers.js
+++ js/Photographers.js
@@ -1,33 +1,35 @@
 class Photographers {
     constructor(){
         this.mainContent = document.getElementById('main-content');
-        this.url = 'data/fisheye_data.json'
+        this.photographersApi = new PhotographerApi('data/fisheye_data.json');
     }
 
-    async getData(){
+    async launchPhotographers(){
 
-        //req settings
-        const request = await fetch(this.url,{
-            method: 'GET'
-        });
+        // //req settings
+        // const request = await fetch(this.url,{
+        //     method: 'GET'
+        // });
     
-        //verification - display
-        if(!request.ok){
-            alert('Un problème est survenu.');
-        } else {
-            const data = await request.json();
+        // //verification - display
+        // if(!request.ok){
+        //     alert('Un problème est survenu.');
+        // } else {
+        //     const data = await request.json();
     
-            data.photographers
-                .map(photographer => new Photographer(photographer))
-                .forEach(photographer => {
-                    const Template = new PhotographerCard(photographer)
-                    this.mainContent.appendChild(
-                        Template.createCard()
-                    )
-                });
-        }
+        //     data.photographers
+        //         .map(photographer => new Photographer(photographer))
+        //         .forEach(photographer => {
+        //             const Template = new PhotographerCard(photographer)
+        //             this.mainContent.appendChild(
+        //                 Template.createCard()
+        //             )
+        //         });
+        // }
+        const photographersData = await this.photographersApi.getPhotographers()
+        console.log(photographersData)
     }
 }
 
 const photographerDeck = new Photographers()
-photographerDeck.getData()
+photographerDeck.launchPhotographers()
