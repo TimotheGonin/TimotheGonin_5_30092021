906ddb36ea9d0dac32a9b3a8e7f4dc36ee1e17e3
TimotheGonin <gonin.timothe@gmail.com>
2021-11-18 11:48:57 +0100

    :tada: feat : adding banner launcher with fetch module

--- js/Banner.js
+++ js/Banner.js
@@ -1,39 +1,50 @@
 class Banner {
     constructor(){
         this.mainContent = document.querySelector('main');
-        this.url = 'data/fisheye_data.json'
+        this.photographersApi = new PhotographerApi('data/fisheye_data.json');
     }
 
-    async getData(){
+    async launchBanner(){
 
-        //req settings
-        const request = await fetch(this.url,{
-            method: 'GET'
-        });
+        // //req settings
+        // const request = await fetch(this.url,{
+        //     method: 'GET'
+        // });
     
-        //verification - display
-        if(!request.ok){
-            alert('Un problème est survenu.');
-        } else {
-            const data = await request.json();
+        // //verification - display
+        // if(!request.ok){
+        //     alert('Un problème est survenu.');
+        // } else {
+        //     const data = await request.json();
             
-            //ID url = ID photographer ?
-            data.photographers
-                .map(photographer => new Photographer(photographer))
-                .forEach(photographer => {
-                    if(id === photographer.id){
-                        const Template = new PhotographerBanner(photographer)
-                        this.mainContent.prepend(
-                        Template.createBanner()
-                        )
-                    }
-                });
-        }
+        //     //ID url = ID photographer ?
+        //     data.photographers
+        //         .map(photographer => new Photographer(photographer))
+        //         .forEach(photographer => {
+        //             if(id === photographer.id){
+        //                 const Template = new PhotographerBanner(photographer)
+        //                 this.mainContent.prepend(
+        //                 Template.createBanner()
+        //                 )
+        //             }
+        //         });
+        // }
+        const photographersData = await this.photographersApi.getPhotographers()
+        photographersData
+            .map(photographer => new Photographer(photographer))
+            .forEach(photographer => {
+                if(id === photographer.id){
+                    const Template = new PhotographerBanner(photographer)
+                    this.mainContent.prepend(
+                    Template.createBanner()
+                    )
+                }
+            });
     }
 }
 
 const photographerDeck = new Banner()
-photographerDeck.getData()
+photographerDeck.launchBanner()
 
 //CREATE URL OBJECT
 let pageUrl = new URL(window.location.href);
