26d19498dd853a87087788463b0bb400e313a14f
TimotheGonin <gonin.timothe@gmail.com>
2021-11-18 14:53:20 +0100

    Merge branch 'feature/fetch-module' into develop

--- js/Medias.js
+++ js/Medias.js
@@ -1,36 +1,25 @@
 class Medias {
     constructor(){
         this.mainContent = document.getElementById('media-cards-deck');
-        this.url = 'data/fisheye_data.json'
+        this.mediasApi = new MediaApi('data/fisheye_data.json');
     }
 
-    async getData(){
+    async launchMedias(){
 
-        //req settings
-        const request = await fetch(this.url,{
-            method: 'GET'
-        });
-    
-        //verification - display
-        if(!request.ok){
-            alert('Un problème est survenu.');
-        } else {
-            const data = await request.json();
-            
-            //ID url = ID photographer ?
-            data.media
-                .map(media => new MediaFactory(media))
-                .forEach(element => {
-                    if(id === element.photographerId){
-                        const Template = new MediaCard(element)
-                        this.mainContent.append(
-                        Template.createMediaCard()
-                        )
-                    }
-                });
-        }
+        const mediasData = await this.mediasApi.getMedias()
+
+        mediasData
+            .map(media => new MediaFactory(media))
+            .forEach(media => {
+                if(id === media.photographerId){
+                    const Template = new MediaCard(media)
+                    this.mainContent.append(
+                    Template.createMediaCard()
+                    )
+                }
+            });
     }
 }
 
 const mediaCardDeck = new Medias()
-mediaCardDeck.getData()
+mediaCardDeck.launchMedias()
--- js/Photographers.js
+++ js/Photographers.js
@@ -1,33 +1,23 @@
 class Photographers {
     constructor(){
         this.mainContent = document.getElementById('main-content');
-        this.url = 'data/fisheye_data.json'
+        this.photographersApi = new PhotographerApi('data/fisheye_data.json');
     }
 
-    async getData(){
+    async launchPhotographers(){
 
-        //req settings
-        const request = await fetch(this.url,{
-            method: 'GET'
-        });
-    
-        //verification - display
-        if(!request.ok){
-            alert('Un problème est survenu.');
-        } else {
-            const data = await request.json();
-    
-            data.photographers
-                .map(photographer => new Photographer(photographer))
-                .forEach(photographer => {
-                    const Template = new PhotographerCard(photographer)
-                    this.mainContent.appendChild(
-                        Template.createCard()
-                    )
-                });
-        }
+        const photographersData = await this.photographersApi.getPhotographers()
+        
+        photographersData
+            .map(photographer => new Photographer(photographer))
+            .forEach(photographer => {
+                const Template = new PhotographerCard(photographer)
+                this.mainContent.appendChild(
+                    Template.createCard()
+                )
+            });
     }
 }
 
 const photographerDeck = new Photographers()
-photographerDeck.getData()
+photographerDeck.launchPhotographers()
