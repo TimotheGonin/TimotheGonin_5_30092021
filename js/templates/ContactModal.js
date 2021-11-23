5352d1014887045ac7689dba60ba87544f51a0d1
TimotheGonin <gonin.timothe@gmail.com>
2021-11-23 12:14:29 +0100

    feat : add input/label layout

--- js/templates/ContactModal.js
+++ js/templates/ContactModal.js
@@ -18,14 +18,20 @@ class ContactModal {
                         <i class="fas fa-times "></i>
                     </span>
                 </header>
-                <form>
-                    <div>
+                <form class="modal--contact__body">
+                    <div class="modal--contact__input-group">
                         <label>Prénom</label>
                         <input type="text"/>
+                    </div>
+                    <div class="modal--contact__input-group">
                         <label>Nom</label>
                         <input type="text"/>
+                    </div>
+                    <div class="modal--contact__input-group">
                         <label>Email</label>
                         <input typ="email"/>
+                    </div>
+                    <div class="modal--contact__input-group">
                         <label>Votre Message</label>
                         <textarea></textarea>
                     </div>
