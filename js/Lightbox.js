7ece477420a6fac79464a8f0ca74d8f1fa22b1c6
TimotheGonin <gonin.timothe@gmail.com>
2021-12-03 14:40:09 +0100

    feat : add image launcher  coming from link

--- js/Lightbox.js
+++ js/Lightbox.js
@@ -1,7 +1,7 @@
 class Ligthbox {
 
     static init (){
-        const links = document.querySelectorAll('a[href$=".png"], a[href$=".jpg"], a[href$=".jpeg"]')
+        const links = document.querySelectorAll('a[href$=".mp4"], a[href$=".png"], a[href$=".jpg"], a[href$=".jpeg"]')
             .forEach(link => link.addEventListener('click', e => {
                 e.preventDefault();
                 new Ligthbox(e.currentTarget.getAttribute('href'));
@@ -13,8 +13,20 @@ class Ligthbox {
      * @param{string} url URL de l'image
      */
     constructor(url){
-        const element = this.buildDOM(url);
-        document.body.appendChild(element);
+        this.element = this.buildDOM(url);
+        this.loadImage(url);
+        document.body.appendChild(this.element);
+    }
+
+    /**
+     * 
+     * @param{string} url URL de l'image
+     */
+    loadImage(url){
+        const image = new Image();
+        const container = this.element.querySelector('.lightbox__container');
+        image.src = url;
+        container.appendChild(image);
     }
 
     /**
@@ -29,7 +41,6 @@ class Ligthbox {
         <button class="lightbox__next">Suivant</button>
         <button class="lightbox__prev">Précédent</button>
         <div class="lightbox__container">
-            <img src="${url}" alt="">
         </div>`;
         return dom;
     }
