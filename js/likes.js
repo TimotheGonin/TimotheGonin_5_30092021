ee3179b301a11a5bfcace0fba24282f84682d175
TimotheGonin <gonin.timothe@gmail.com>
2021-12-08 10:49:06 +0100

    :tada: feat : function to increment media-card's likes

--- js/likes.js
+++ js/likes.js
@@ -1,22 +1,12 @@
 function liking(){
 
     //Show me the DOM
-    const counterBox = this.querySelector('.picture-card__counter-like');
-    const counter = Number(this.querySelector('.picture-card__counter-like p').textContent);
-    console.log(counter);
+    const counterElement = this.querySelector('.picture-card__counter-like p');
+    const counterValue = Number(this.querySelector('.picture-card__counter-like p').textContent);
 
     //Increment
-    let newCounter = document.createElement('p');
-    newCounter.textContent = `${counter + 1}`;
+    counterElement.textContent = `${counterValue + 1}`;
 
-    counterBox.removeChild(counter);
-    
-    // const counter = document.querySelector('.picture-card__counter-like p');
-    // const newCounter = document.createElement('p');
-    // newCounter.innerText = `New Value Lauched`;
-    // counterBox.removeChild(counter);
-    // counterBox.appendChild(newCounter);
-    // console.log(counterBox);
 }
 
 
