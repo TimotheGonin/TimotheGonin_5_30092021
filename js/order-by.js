78581637bc641094e2af661eb0717cdfe63be82d
TimotheGonin <gonin.timothe@gmail.com>
2021-12-09 16:33:24 +0100

    :construction: test: sibling elements needed + test select target value

--- js/order-by.js
+++ js/order-by.js
@@ -1,2 +1,18 @@
+// ┌──────────────────────────────────────────────────────────────────────────────┐
+// │ ELEMENTS                                                                     │
+// └──────────────────────────────────────────────────────────────────────────────┘
+
+// ORDERBY BOX/BUTTON
 const orderByButton = document.querySelector('#orderBy');
-orderByButton.addEventListener('click', ()=> console.log('OK orderByButton'));
+const gallery = document.querySelector('#media-cards-deck');
+const galleryContent = document.querySelectorAll('.picture-card');
+console.log(galleryContent);
+galleryContent.forEach(elt => elt.style.opacity='0.4');
+
+orderByButton.addEventListener('change',  (e) => {
+    console.log(e.target.value);
+
+    // if(e.target.value === 'Date'){
+    //     gallery.style.flexDirection='row-reverse';
+    // }
+});
