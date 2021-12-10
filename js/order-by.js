4017790d7f6d0f87f3b4526bd337a4735eb9e17e
TimotheGonin <gonin.timothe@gmail.com>
2021-12-10 14:15:17 +0100

    :construction: test: apply changes from orderBy Button value

--- js/order-by.js
+++ js/order-by.js
@@ -3,16 +3,30 @@
 // └──────────────────────────────────────────────────────────────────────────────┘
 
 // ORDERBY BOX/BUTTON
-const orderByButton = document.querySelector('#orderBy');
-const gallery = document.querySelector('#media-cards-deck');
-const galleryContent = document.querySelectorAll('.picture-card');
+const orderByButton = document.querySelector('#orderBy'); 
+const gallery = document.querySelector('#media-cards-deck'); //media-cards collection container
+const galleryContent = document.querySelectorAll('.picture-card'); //media-cards collection
 console.log(galleryContent);
-galleryContent.forEach(elt => elt.style.opacity='0.4');
 
 orderByButton.addEventListener('change',  (e) => {
     console.log(e.target.value);
-
-    // if(e.target.value === 'Date'){
-    //     gallery.style.flexDirection='row-reverse';
-    // }
+    gallery.innerHTML ='';
+    
+    console.log(galleryContent);
+    if(e.target.value === 'Popularité'){
+        galleryContent.forEach(elt => {
+            elt.style.background='red';
+            gallery.appendChild(elt);
+        });
+    } else if(e.target.value === 'Date'){
+        galleryContent.forEach(elt => {
+            elt.style.background='orange';
+            gallery.appendChild(elt);
+        });
+    } else if(e.target.value === 'Titre'){
+        galleryContent.forEach(elt => {
+            elt.style.background='green';
+            gallery.appendChild(elt);
+        });
+    }
 });
